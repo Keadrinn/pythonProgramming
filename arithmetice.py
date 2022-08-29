bc94fec5e65170432c2007b7b482d2e587000fc4
Keadrinn <111439593+Keadrinn@users.noreply.github.com>
2022-08-29 08:55:50 -0500

    Add files via upload

---
+++ arithmetice.py
@@ -0,0 +1,15 @@
+# Programmer : Keadrin Allen
+# Program : Arithmetic operations
+# First created : 08/29/2022 8:32:00
+# Last changed : 08/29/2022 8:32:00
+# Version : Pyhton 3.10.6
+
+print("Arithmetic Operations")
+firstInput = 5
+secondInput = 10
+
+print("sum of given numbers is ",firstInput+secondInput)
+print("differnce of given numbers is ",firstInput-secondInput)
+print("product of given numbers is ",firstInput*secondInput)
+print("quotient of given numbers is ",firstInput/secondInput)
+print("remainder of given numbers is ",firstInput%secondInput) 
